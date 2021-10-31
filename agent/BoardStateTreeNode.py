3ec264e1f44b2408d4bad9e742f59e778682de73
Brandon Fraune <brandon.fraune@directsupply.com>
2021-10-30 21:09:45 -0500

    tree updates from testing

--- agent/BoardStateTreeNode.py
+++ agent/BoardStateTreeNode.py
@@ -8,7 +8,7 @@ from agent import Scorer
 class BoardStateTreeNode:
     _board: chess.Board
     _move: chess.Move
-    _children: list['BoardStateTreeNode'] = []
+    _children: list['BoardStateTreeNode']
     _is_end_game: bool
     _turn_color: bool  # True if white, False if black
     _max_children: int  # Limit width of tree
@@ -16,28 +16,55 @@ class BoardStateTreeNode:
     def __init__(self, board: chess.Board, move: chess.Move = None, max_children: int = None):
         self._board = board
         self._move = move
+        self._children = []
+        self._is_end_game = board.is_game_over()
+        self._turn_color = board.turn == chess.WHITE
         self._max_children = max_children
 
     def populate_tree(self, depth: int):
         if depth <= 0:
+            print('populate_tree: Reached depth base case.')
             return
 
-        moves = self._board.legal_moves()
-        while len(moves) > self._max_children:
-            index_to_remove = random.randint(0, len(moves) - 1)
-            moves.pop(index_to_remove)
+        if self._board.is_game_over():
+            print('populate_tree: No more moves to populate. Game is over at current node.')
+            return
 
+        moves = []
+        for move in self._board.legal_moves:
+            if self._max_children is None:
+                moves.append(move)
+
+        # Reduce move set to num max children
+        print(f'populate_tree: {len(moves)} legal moves found')
+        if self._max_children is not None:
+            while len(moves) > self._max_children:
+                index_to_remove = random.randint(0, len(moves) - 1)
+                moves.pop(index_to_remove)
+
+        print(f'populate_tree: {len(moves)} moves to be added as children')
         for move in moves:
-            child_board = self._board.copy().push(move)  # push move here?
+            child_board = self._board.copy()
+            child_board.push(move)  # push move here?
             child = BoardStateTreeNode(child_board, move, self._max_children)
             child.populate_tree(depth - 1)
             self._children.append(child)
 
-    def enumerate_moves(self) -> list[chess.Move]:
-        # TODO: write this
-        print('enum moves')
-
     def evaluate_moves(self, score_func: Scorer, agg_func) -> list[(float, chess.Move)]:
-        # what is agg func?
-        # TODO: write this
-        print('eval moves')
+        # might be more efficient to evaluate moves when the tree is built
+        # agg func is _gather_leaf_scores?
+        my_options = []
+        for child in self._children:
+            my_options.append((self._gather_leaf_scores(child, score_func), child._move))
+        return my_options
+
+    def enumerate_moves(self) -> list[chess.Move]:
+        return self._board.legal_moves
+
+    def _gather_leaf_scores(self, node: 'BoardStateTreeNode', score_func: Scorer) -> float:
+        my_score = score_func.evaluate(node)
+        scores = [my_score]
+        for child in node._children:
+            scores.append(self._gather_leaf_scores(child, score_func))
+
+        return sum(scores) / len(scores)
---
+++ tests/test_BoardStateTreeNode.py
@@ -0,0 +1,85 @@
+import io
+import sys
+from unittest import TestCase
+
+import chess
+from pytest import raises
+
+from agent.BoardStateTreeNode import BoardStateTreeNode
+from agent.Scorer import Scorer
+
+
+class TestBoardStateTreeNode(TestCase):
+    def test_initial_state(self):
+        # Arrange
+        board = chess.Board()
+        root_node = BoardStateTreeNode(board)
+        # Assert
+        assert root_node._board == board
+        assert root_node._move is None
+        assert root_node._children == []
+        assert root_node._is_end_game is False
+        assert root_node._turn_color is True
+        assert root_node._max_children is None
+
+    def test_populate_tree_negative_depth_returns(self):
+        board = chess.Board()
+        root_node = BoardStateTreeNode(board)
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        # Act
+        root_node.populate_tree(-1)
+        sys.stdout = sys.__stdout__  # Reset redirect.
+
+        # Assert
+        assert captured_output.getvalue() == 'populate_tree: Reached depth base case.\n'
+
+    def test_populate_tree_game_over_returns(self):
+        # Arrange
+        game_over_fen = '2Q2k2/1R6/7R/1P2P1P1/P6P/8/8/1NB1KBN1 b - - 4 37'
+        board = chess.Board(game_over_fen)
+        root_node = BoardStateTreeNode(board)
+
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        # Act
+        root_node.populate_tree(5)
+        sys.stdout = sys.__stdout__  # Reset redirect.
+
+        # Assert
+        assert captured_output.getvalue() == 'populate_tree: No more moves to populate. Game is over at current node.\n'
+
+    def test_populate_tree_two_children(self):
+        # Arrange
+        game_nearly_over_fen = '4k3/1R6/2Q4R/1P2P1P1/P6P/8/8/1NB1KBN1 b - - 2 36'
+        board = chess.Board(game_nearly_over_fen)
+        root_node = BoardStateTreeNode(board)
+
+        # Act
+        root_node.populate_tree(5)
+
+        # Assert
+        assert len(root_node._children) == 2
+
+    # def test_evaluate_moves_equal_weights(self):
+    #     # Arrange
+    #     game_nearly_over_fen = '4k3/1R6/2Q4R/1P2P1P1/P6P/8/8/1NB1KBN1 b - - 2 36'
+    #     board = chess.Board(game_nearly_over_fen)
+    #     root_node = BoardStateTreeNode(board)
+    #
+    #     # Act
+    #     root_node.populate_tree(5)
+    #     moves = root_node.evaluate_moves(Scorer(), None)
+    #
+    #     # Assert
+    #     assert len(root_node._children) == 2
+    #     assert moves[0][0] == moves[1][0]
+
+    # def test_enumerate_moves(self):
+    #     self.fail()
+    #
+    # def test__gather_leaf_scores(self):
+    #     self.fail()
