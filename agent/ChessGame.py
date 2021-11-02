25af38c4eb9403b2926ee541aa0c623106217d92
Brandon Fraune <brandon.fraune@directsupply.com>
2021-11-01 22:20:38 -0500

    cleaning up

--- agent/ChessGame.py
+++ agent/ChessGame.py
@@ -5,30 +5,13 @@ from agent.Player import RandomPlayer, SearchPlayer
 
 
 class ChessGame:
-    _board: chess.Board
+    _board: chess.Board = chess.Board()
     # _played_turns: int  # redundant with Board.fullmove_number
     _black_player: Player = RandomPlayer()
-    _white_player: Player = SearchPlayer()
-
-    def play_turn(self) -> bool:
-        # TODO: play
-
-        return self._board.is_game_over()
+    _white_player: Player = SearchPlayer(3, 15)
 
     def reset(self):
         self._board.reset()
-        # More to do here?
-
-    def print_game_stats(self):
-        print(f'Game Status: {self._game_status()}')
-        print(f'  Whose turn  = {self._whose_turn_color()}')
-        print(f'  Turns taken = {self._board.fullmove_number}')  # Increments only after black moves
-        print(f'  Is check    = {self._board.is_check()}')  # Increments only after black moves
-
-        if self._board.is_game_over():
-            print(f'  Checkmate?   = {self._board.is_checkmate()}')  # Increments only after black moves
-            print(f'  Stalemate?   = {self._board.is_stalemate()}')  # Increments only after black moves
-            print(f'  Winner color = {self._board.outcome().winner}')  # Increments only after black moves
 
     def play_until(self, n_turns: int) -> bool:
         for index in range(1, n_turns + 1):
@@ -36,6 +19,27 @@ class ChessGame:
             if game_over:
                 print('Game over')
                 return True
+
+    def play_turn(self) -> bool:
+        move = self._white_player.get_next_move(self._board) \
+            if self._board.turn else self._black_player.get_next_move(self._board)
+
+        if move:
+            print(move)
+            self._board.push(move)
+        return self._board.is_game_over()
+
+    def print_game_stats(self):
+        print(f'Game Status: {self._game_status()}')
+        print(f'  Whose turn  = {self._whose_turn_color()}')
+        print(f'  Turns taken = {self._board.fullmove_number}')  # Increments only after black moves
+        print(f'  Is check    = {self._board.is_check()}')
+
+        if self._board.is_game_over():
+            print(f'  Checkmate?   = {self._board.is_checkmate()}')
+            print(f'  Stalemate?   = {self._board.is_stalemate()}')
+            print(f'  Winner color = {self._board.outcome().winner}')
+            print(f'  Outcome      = {self._board.outcome()}')
 
     def _game_status(self) -> str:
         return 'game over' if self._board.is_game_over() else 'in progress'
