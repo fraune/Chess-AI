d0a10d03897b962a4ed6820556147b8b5d89e0a3
Brandon Fraune <brandon.fraune@directsupply.com>
2021-11-04 18:59:48 -0500

    print a time report for the full simulation set

--- utility/simulator.py
+++ utility/simulator.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from agent.ChessGame import ChessGame
 from config import GAMES_TO_SIMULATE, MAXIMUM_PLIES_PER_GAME
 from utility.Logger import Logger
@@ -8,15 +10,22 @@ logger = Logger()
 
 def run_simulation_set():
     sw = SummaryWriter()
+    start_datetime = datetime.now()
 
     for simulation in range(1, GAMES_TO_SIMULATE + 1):
+        print(f'Starting game {simulation}/{GAMES_TO_SIMULATE}')
         summary = run_one_simulation(simulation)
         dict_to_write = {'simulation': simulation, **summary}
         sw.write_summary(dict_to_write)
 
+    end_datetime = datetime.now()
+    time_delta = end_datetime - start_datetime
+    avg_time = time_delta.total_seconds() / GAMES_TO_SIMULATE
+    print(f'Ran {GAMES_TO_SIMULATE} simulations in {str(time_delta)},'
+          f'for an average of {avg_time:.6f} seconds per game.')
+
     logger.flush()
     logger.close()
-
     sw.flush()
     sw.close()
 
