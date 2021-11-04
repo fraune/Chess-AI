59dfeab985da30082311bee05a3a6b7515d63cf3
Brandon Fraune <brandon.fraune@directsupply.com>
2021-11-04 12:12:31 -0500

    created a logger singleton that can print to files

---
+++ utility/logger.py
@@ -0,0 +1,40 @@
+import datetime
+
+
+class Logger(object):
+    _instance = None
+    _print_to_console = True
+    _print_to_file = False
+    _file = None
+
+    def __new__(cls, print_to_console: bool = True, print_to_file: bool = False, file_name: str = None):
+        if cls._instance is None:
+            cls._instance = super(Logger, cls).__new__(cls)
+            cls._instance._initialize(print_to_console, print_to_file, file_name)
+        return cls._instance
+
+    def _initialize(self, print_to_console: bool = True, print_to_file: bool = False, file_name: str = None):
+        self._print_to_console = print_to_console
+        if print_to_file:
+            self._file = open(file_name, 'a')
+            self._print_to_file = print_to_file
+            self.log('Logger initialized')
+
+    def log(self, text: str, flush: bool = False):
+        now = datetime.datetime.now()
+        message = f'[{now}] {text}'
+        if self._print_to_console:
+            print(message)
+        if self._print_to_file:
+            self._file.write(f'{message}\n')
+            if flush:
+                self._file.flush()
+
+    def flush(self):
+        if self._print_to_file:
+            self._file.flush()
+
+    def close(self):
+        if self._print_to_file:
+            self._print_to_file = False
+            self._file.close()
