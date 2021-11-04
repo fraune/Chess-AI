9e673e2601c5d2e6a8d8e225452a90df768e6236
Brandon Fraune <brandon.fraune@directsupply.com>
2021-11-04 15:52:39 -0500

    added type hints

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ LOG_FILE_NAME: str = None  # Only used if PRINT_TO_FILE is set. Overrides Logger
 SUMMARY_FILE_NAME: str = None  # Overrides SummaryWriter's automatic file name generation.
 
 # utility.simulator
-GAMES_TO_SIMULATE = 3
-MAXIMUM_PLIES_PER_GAME = 1000  # Most "Random VS Random" games are completed in less than 300 total plies
+GAMES_TO_SIMULATE: int = 3
+MAXIMUM_PLIES_PER_GAME: int = 1000  # Most "Random VS Random" games are completed in less than 300 total plies
