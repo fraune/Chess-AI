27ca03b233d9d22f043f29c77d5b3c84df6937b0
Brandon Fraune <brandon.fraune@directsupply.com>
2021-11-04 14:33:23 -0500

    automate running multiple simulations and reporting out the data

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from agent.ChessGame import ChessGame
-from utility.logger import Logger
+from utility.Logger import Logger
+from utility.SummaryWriter import SummaryWriter
 
 
-def start():
-    logger = Logger(print_to_file=True)
+def run_simulation(sim_number: int = 1):
+    logger = Logger(print_to_file=False)
+    logger.log(f'Simulation {sim_number} beginning')
 
     game = ChessGame()
-    game.play_until(1)
+    game.play_until(999)
 
+    logger.log(f'Simulation {sim_number} complete')
     logger.close()
 
+    return game.summary()
 
-start()
+
+def run_simulation_set(sets_to_run: int):
+    sw = SummaryWriter()
+    for simulation in range(1, sets_to_run + 1):
+        summary = run_simulation(simulation)
+        dict_to_write = {'simulation': simulation, **summary}
+        sw.write_summary(dict_to_write)
+    sw.flush()
+    sw.close()
+
+
+run_simulation_set(1000)
