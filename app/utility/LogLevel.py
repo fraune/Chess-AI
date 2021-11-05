19f6df74817f34f167c48213a19c3a6a81847143
Brandon Fraune <brandon.fraune@directsupply.com>
2021-11-04 22:56:44 -0500

    Introduce a log level to filter logs that get printed on the console

---
+++ app/utility/LogLevel.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class LogLevel(Enum):
+    VERBOSE = 1
+    DEBUG = 2
+    INFO = 3
+    ERROR = 4
--- app/utility/Logger.py
+++ app/utility/Logger.py
@@ -1,35 +1,39 @@
 from datetime import datetime
 
-from app.config import PRINT_TO_CONSOLE, PRINT_TO_FILE, LOG_FILE_NAME
+from app.config import PRINT_TO_CONSOLE, PRINT_TO_FILE, LOG_FILE_NAME, LOG_LEVEL_FILTER
+from app.utility.LogLevel import LogLevel
 
 
 class Logger(object):
     _instance = None  # static
 
     _print_to_console = True
+    _log_level_filter: LogLevel
     _print_to_file = False
     _file = None
 
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(Logger, cls).__new__(cls)
-            cls._instance._initialize(PRINT_TO_CONSOLE, PRINT_TO_FILE, LOG_FILE_NAME)
+            cls._instance._initialize(PRINT_TO_CONSOLE, LOG_LEVEL_FILTER, PRINT_TO_FILE, LOG_FILE_NAME)
         return cls._instance
 
-    def _initialize(self, print_to_console: bool = True, print_to_file: bool = False, file_name: str = None):
+    def _initialize(self, print_to_console: bool = True, log_level_filter: LogLevel = LogLevel.VERBOSE,
+                    print_to_file: bool = False, file_name: str = None):
         self._print_to_console = print_to_console
+        self._log_level_filter = log_level_filter
         if print_to_file:
             if not file_name:
                 now = datetime.now().strftime('%Y%m%d%H%M%S')
                 file_name = f'{now}-chess-game.txt'
             self._file = open(f'../output/{file_name}', 'a')
             self._print_to_file = print_to_file
-            self.log('Logger initialized')
+        self.log('Logger initialized')
 
-    def log(self, data, flush: bool = False):
+    def log(self, data, log_level: LogLevel = LogLevel.VERBOSE, flush: bool = False):
         now = datetime.now()
         message = f'[{now}] {data}'
-        if self._print_to_console:
+        if self._print_to_console and log_level.value >= self._log_level_filter.value:
             print(message)
         if self._print_to_file:
             self._file.write(f'{message}\n')
