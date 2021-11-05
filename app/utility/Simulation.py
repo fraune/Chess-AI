cd2d6e9ad0251378ace344ca758568bbfae2c859
Brandon Fraune <brandon.fraune@directsupply.com>
2021-11-04 23:15:30 -0500

    Fix circular import

--- app/utility/Simulation.py
+++ app/utility/Simulation.py
@@ -1,10 +1,11 @@
+from typing import Optional
+
 from app.agent.PlayerType import PlayerType
-from app.config import MAXIMUM_TREE_WIDTH, MAXIMUM_TREE_DEPTH
 
 
 class Simulation:
     def __init__(self, matches: int, white_player_type: PlayerType, black_player_type: PlayerType,
-                 search_depth: int = MAXIMUM_TREE_DEPTH, search_width: int = MAXIMUM_TREE_WIDTH):
+                 search_depth: Optional[int] = None, search_width: Optional[int] = None):
         self.matches = matches
         self.white_player_type = white_player_type
         self.black_player_type = black_player_type
