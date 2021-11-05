e6ef1cb8230ff21af054acb8d2757c2406327603
Brandon Fraune <brandon.fraune@directsupply.com>
2021-11-04 23:15:49 -0500

    pull out large experiment into separate file

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from typing import Optional
 from app.agent.PlayerType import PlayerType
 from app.utility.LogLevel import LogLevel
 from app.utility.Simulation import Simulation
+from app.utility.experiment import experiment
 
 # utility.Logger.Logger
 PRINT_TO_CONSOLE: bool = True
@@ -15,10 +16,7 @@ LOG_LEVEL_FILTER: LogLevel = LogLevel.INFO
 v1_GAMES_TO_SIMULATE: int = 10  # Random vs Random ~1000 games per minute?
 v1_MAXIMUM_PLIES_PER_GAME: int = 1000  # Most "Random VS Random" games are completed in less than 300 total plies
 #   Affects v2 simulations only
-v2_SIMULATION_CONFIGURATIONS: list[Simulation] = [
-    Simulation(10, PlayerType.RANDOM, PlayerType.RANDOM),
-    Simulation(100, PlayerType.SEARCH, PlayerType.RANDOM, 1, 1)
-]
+v2_SIMULATION_CONFIGURATIONS: list[Simulation] = experiment
 
 # agent.ChessGame.ChessGame
 #   Affects v1 simulations only
@@ -26,5 +24,6 @@ WHITE_PLAYER_TYPE: PlayerType = PlayerType.RANDOM
 BLACK_PLAYER_TYPE: PlayerType = PlayerType.RANDOM  # Haven't validated Search with Black
 
 # agent.Player.SearchPlayer
+#   Affects v1 simulations only
 MAXIMUM_TREE_DEPTH: int = 2  # Factor to limit the number of plies to look ahead
 MAXIMUM_TREE_WIDTH: int = 5  # Factor to limit the number of possible moves per ply
